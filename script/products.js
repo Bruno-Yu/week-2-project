fb0ead6d3fc2e993c5a04a0a517f2477de79dfdf
Bruno-Yu <jackhellowin@gmail.com>
2022-01-21 15:49:44 +0800

    week-2-project-v2
    測試用token改成從cookie中取出

--- script/products.js
+++ script/products.js
@@ -9,6 +9,7 @@ const app={
             name:"我建好了Vue原件",
             url:'https://vue3-course-api.hexschool.io/v2',
             path:'brunoyu2022',
+            // 測試用token
             token:"eyJhbGciOiJSUzI1NiIsImtpZCI6InRCME0yQSJ9.eyJpc3MiOiJodHRwczovL3Nlc3Npb24uZmlyZWJhc2UuZ29vZ2xlLmNvbS92dWUtY291cnNlLWFwaSIsImF1ZCI6InZ1ZS1jb3Vyc2UtYXBpIiwiYXV0aF90aW1lIjoxNjQyNjc5MTE3LCJ1c2VyX2lkIjoiQTlFUkZrVXZCZmRySFl5eTJaMXAwV1djbTZnMSIsInN1YiI6IkE5RVJGa1V2QmZkckhZeXkyWjFwMFdXY202ZzEiLCJpYXQiOjE2NDI2NzkxMTcsImV4cCI6MTY0MzExMTExNywiZW1haWwiOiJicnVub3l1MjAyMkBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiYnJ1bm95dTIwMjJAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.R81nd64OXXcEls2DcjrZwLAUQ8K7_NVFe6Zanr2M8ByeFPZ2Kkgwz3sf6RbuY__nAfT7VYiZQe3vMYGNYd_KaZq5KFAlVpM1a14I0UP4xaSFnURJJZ0hzgKd3ZYkMMynErxTbKSXM8InMV-2i8n7ALtxckussK69YPrUIhmtm61hdKzf94w3PiuKUhx-RCpURhTvVx_1gwa0Kg07JTNHOyirwaovVd2KZBVxpDbX7AfAzG3tYS7hCizXckFh_W2WUxDwphpg63bjeLc2QTSpK28iPHEqH7fdVlRKypC9D_sY28OOYvwSimQuzRYpSVdBUiC2nfVg1YIjHhHnE7fIFw ",
             expires:1643111117219,
             products:[],
@@ -45,7 +46,10 @@ const app={
 
         // 測試時，先用data內測試用的token
         // 將該token及入header中使用post發送
-        axios.defaults.headers.common['Authorization'] = this.token;
+        // axios.defaults.headers.common['Authorization'] = this.token;
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)hextoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        axios.defaults.headers.common['Authorization'] = token;
+
         axios.post(`${this.url}/api/user/check`)
         // 成功結果接收
         .then((res)=>{
@@ -70,10 +74,10 @@ const app={
           })
 
 
-        // 在瀏覽器中取cookie中的token
+        // // 在瀏覽器中取cookie中的token
         // axios.defaults.headers.common['Authorization'] = token;
         // const token = document.cookie.replace(/(?:(?:^|.*;\s*)hextoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-        // 確認是否有成功登入
+        // // 確認是否有成功登入
         
 
 
