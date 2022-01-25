57a683e082366f114daf85b155f562e13f99350e
Bruno-Yu <jackhellowin@gmail.com>
2022-01-25 21:18:12 +0800

    wee-2-project-renewed
    1. 登入頁已改成vue元件寫法
    2. 產品頁:
        a.  “未啟用” 在 class 加上樣式 text-danger;
        b. 將AJAX get 寫在驗證成功的then執行
       c. computed處理的total 對應的html內容修正
       d. 移除測試用token跟 expires

--- script/index.js
+++ script/index.js
@@ -9,42 +9,93 @@
 // 回傳catch資訊讓使用者知道輸入錯誤的提示
 
 
-// 注意: 原button內預設submit type的轉址傳送功能 需研究一下
-
-// 加入api站點、路徑
-const url = 'https://vue3-course-api.hexschool.io/v2'; // 請加入站點
-const path = 'brunoyu2022'; // 請加入個人 API Path
-
-// 登入按鈕DOM元素
-const loginBtn=document.querySelector('button[data-name="loginBtn"]');
-// EMAIL 輸入欄DOM元素
-const InputEmail = document.querySelector('#username')
-// PASSWORD輸入欄DOM元素
-const InputPassword = document.querySelector('#password')
-// 注意監聽的第二個函式只能寫名字不能加()否則會直接執行
-loginBtn.addEventListener('click', login);
-function login(){
-    console.log('有點到按鈕');
-    // 抓取使用者輸入資訊
-    const username=InputEmail.value;
-    const password=InputPassword.value;
-    const user={username, password};
-    console.log(user);
-    // 使用axios與傳送api確認授權
-    axios.post(`${url}/admin/signin`, user)
-    .then((res)=>{ //成功登入設置
-        console.log("登入成功");
-        //存入cookie
-        const {token, expired }=res.data;//分解宣告
-        console.log(token, expired);
-        document.cookie = `hextoken=${token}; expires=${expired};`;
-        alert('登入成功，按確定後進行跳轉');
-        // 轉址
-        window.location.href='products.html';
-    })
-    .catch((error)=>{
-        alert(`警告: ${error.data.message} \n提醒: ${error.data.error.message}`)
-    })
 
 
-};
+// // 加入api站點、路徑
+// const url = 'https://vue3-course-api.hexschool.io/v2'; // 請加入站點
+// const path = 'brunoyu2022'; // 請加入個人 API Path
+
+// // 登入按鈕DOM元素
+// const loginBtn=document.querySelector('button[data-name="loginBtn"]');
+// // EMAIL 輸入欄DOM元素
+// const InputEmail = document.querySelector('#username')
+// // PASSWORD輸入欄DOM元素
+// const InputPassword = document.querySelector('#password')
+// // 注意監聽的第二個函式只能寫名字不能加()否則會直接執行
+// loginBtn.addEventListener('click', login);
+// function login(){
+//     console.log('有點到按鈕');
+//     // 抓取使用者輸入資訊
+//     const username=InputEmail.value;
+//     const password=InputPassword.value;
+//     const user={username, password};
+//     console.log(user);
+//     // 使用axios與傳送api確認授權
+//     axios.post(`${url}/admin/signin`, user)
+//     .then((res)=>{ //成功登入設置
+//         console.log("登入成功");
+//         //存入cookie
+//         const {token, expired }=res.data;//分解宣告
+//         console.log(token, expired);
+//         document.cookie = `hextoken=${token}; expires=${expired};`;
+//         alert('登入成功，按確定後進行跳轉');
+//         // 轉址
+//         window.location.href='products.html';
+//     })
+//     .catch((error)=>{
+//         alert(`警告: ${error.data.message} \n提醒: ${error.data.error.message}`)
+//     })
+
+
+// };
+
+
+// vue元件寫法
+
+const app=Vue.createApp({
+    // 資料集 data
+    data(){
+        return {
+            url:"https://vue3-course-api.hexschool.io/v2" ,
+            path:"brunoyu2022",
+
+        };
+    },
+    // 方法集 methods
+    methods:{
+        // 登入鈕
+        login(){
+            console.log('有點到按鈕');
+            // 抓取使用者輸入資訊
+            // 登入按鈕DOM元素
+            const loginBtn=document.querySelector('button[data-name="loginBtn"]');
+            // EMAIL 輸入欄DOM元素
+            const InputEmail = document.querySelector('#username')
+            // PASSWORD輸入欄DOM元素
+            const InputPassword = document.querySelector('#password')
+            const username=InputEmail.value;
+            const password=InputPassword.value;
+            const user={username, password};
+            console.log(user);
+            // 使用axios與傳送api確認授權
+            axios.post(`${this.url}/admin/signin`, user)
+            .then((res)=>{ //成功登入設置
+                console.log("登入成功");
+                //存入cookie
+                const {token, expired }=res.data;//分解宣告
+                console.log(token, expired);
+                document.cookie = `hextoken=${token}; expires=${expired};`;
+                alert('登入成功，按確定後進行跳轉');
+                // 轉址
+                window.location.href='products.html';
+            })
+            .catch((error)=>{
+                alert(`警告: ${error.data.message} \n提醒: ${error.data.error.message}`)
+            })
+    }
+    },
+    mounted(){
+
+    }
+});
+app.mount('#app');
